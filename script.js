61a21944504652a59e186e120bbcaa0c98492a4c
Dodo20987 <jaydenthung@hotmail.com>
2023-11-20 14:11:30 -0800

    added buttons that move between files fixes #5

---
+++ script.js
@@ -0,0 +1,21 @@
+function goToPage1() {
+    window.location.href = "index.html";
+}
+
+function goToPage2() {
+   
+    window.location.href = "resume-and-skills.html";
+   
+}
+
+function goToPage3() {
+   
+    window.location.href = "hobbies.html";
+   
+}
+function goToPage4() {
+   
+    window.location.href = "programming.html";
+   
+}
+
